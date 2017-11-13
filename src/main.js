6a930de7b3022fe0d87ec9ad92d7ec946b68c6eb
Maxence POUTORD <maxence.poutord@gmail.com>
2017-11-13 13:40:36 +0000

    Sync router with vuex - #4

--- src/main.js
+++ src/main.js
@@ -4,9 +4,12 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import store from './store'
+import { sync } from 'vuex-router-sync'
 import './main.scss'
 
 Vue.config.productionTip = false
+
+sync(store, router)
 
 /* eslint-disable no-new */
 new Vue({
