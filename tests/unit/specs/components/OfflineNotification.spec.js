83815062f9b0c752ede567cd04fc868460ee4ca1
Maxence POUTORD <maxence.poutord@gmail.com>
2017-12-20 14:47:44 +0000

    add tests OfflineNotification.vue

---
+++ tests/unit/specs/components/OfflineNotification.spec.js
@@ -0,0 +1,27 @@
+import { mount } from 'vue-test-utils'
+import OfflineNotification from '@/components/OfflineNotification.vue'
+import store from '@/store'
+
+describe('OfflineNotification.vue', () => {
+  it('test initial rendering (when online)', () => {
+    const wrapper = mount(OfflineNotification, {
+      store,
+      propsData: {
+        isOffline: false
+      }
+    })
+    const template = wrapper.html()
+    expect(template).toMatchSnapshot()
+  })
+
+  it('test initial rendering (when offline)', () => {
+    const wrapper = mount(OfflineNotification, {
+      store,
+      propsData: {
+        isOffline: true
+      }
+    })
+    const template = wrapper.html()
+    expect(template).toMatchSnapshot()
+  })
+})
