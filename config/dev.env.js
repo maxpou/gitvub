6679c1fe7fab7a867ddc54b24bbf8b9198eb2c21
Maxence POUTORD <maxence.poutord@gmail.com>
2017-12-20 09:25:54 +0000

    add secrets to prod (was previously only on dev)

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,7 +3,5 @@ const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"',
-  GITHUB_CLIENTID: '"2c1a8680d4e497d52a52"',
-  GITHUB_CLIENTSECRET: '"9c64d4b112a250806911ed8ba9c3a76e1afbfab2"'
+  NODE_ENV: '"development"'
 })
--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,7 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  GITHUB_URL: '"https://api.github.com"'
+  GITHUB_URL: '"https://api.github.com"',
+  GITHUB_CLIENTID: '"2c1a8680d4e497d52a52"',
+  GITHUB_CLIENTSECRET: '"9c64d4b112a250806911ed8ba9c3a76e1afbfab2"'
 }
