00299fef6ba0cdbf22dc95491d650279e06d6849
Maxence POUTORD <maxence.poutord@gmail.com>
2017-12-28 16:22:33 +0000

    assetsPublicPath part 2

--- config/index.js
+++ config/index.js
@@ -11,7 +11,8 @@ module.exports = {
     index: path.resolve(__dirname, '../dist/index.html'),
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    // --gh-pages option for gh-pages deployment
+    assetsPublicPath: process.env.npm_config_gh_pages ? '/gitvub' : '/',
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
